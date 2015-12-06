286a5fe9745e738ad9052b26c2d45210b33e5bc4
tek <torstenschmits@gmail.com>
2015-12-06 19:36:12 +0100

    Map.{find, values}

--- tryp/map.py
+++ tryp/map.py
@@ -1,14 +1,21 @@
-from typing import TypeVar, Dict, Generic, Tuple
+from typing import TypeVar, Dict, Generic, Tuple, Callable
 
 from toolz import dicttoolz  # type: ignore
 
-from tryp.maybe import may
+from tek.tools import find  # type: ignore
+
+from tryp.maybe import may, Maybe
+from tryp.list import List
 
 A = TypeVar('A')
 B = TypeVar('B')
 
 
 class Map(Dict[A, B], Generic[A, B]):  # type: ignore
+
+    @staticmethod
+    def wrap(d: Dict[A, B]) -> 'Map[A, B]':
+        return Map(d)
 
     @may
     def get(self, key):
@@ -19,5 +26,13 @@ class Map(Dict[A, B], Generic[A, B]):  # type: ignore
 
     def __pow__(self, other: 'Map[A, B]'):
         return Map(dicttoolz.merge(self, other))
+
+    def find(self, f: Callable[[B], bool]) -> Maybe[Tuple[A, B]]:
+        return Maybe(find(lambda a: f(self[a]), self.keys()))\
+            .map(lambda k: (k, self[k]))
+
+    @property
+    def values(self):
+        return List(*Dict.values(self))
 
 __all__ = ['Map']
