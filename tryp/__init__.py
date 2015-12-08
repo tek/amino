9e1fa8edc64fa48bf2fc05d6c77d2e5381707da8
tek <torstenschmits@gmail.com>
2015-12-08 19:02:34 +0100

    convenience forward-import for fn._

--- tryp/__init__.py
+++ tryp/__init__.py
@@ -1,5 +1,8 @@
+from fn import _  # type: ignore
+
 from tryp.maybe import Maybe, Just, Empty, may
 from tryp.list import List
 from tryp.map import Map
 
-__all__ = ['Maybe', 'Just', 'Empty', 'may', 'List', 'Map']
+
+__all__ = ['Maybe', 'Just', 'Empty', 'may', 'List', 'Map', '_']
