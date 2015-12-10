757c3ee3ba47e9b9d83d960848cc3f50c8aeff11
tek <torstenschmits@gmail.com>
2015-12-10 21:06:12 +0100

    logging setup

---
+++ tryp/logging.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+from pathlib import Path  # type: ignore
+
+from lazy import lazy  # type: ignore
+
+import tryp
+
+log = tryp_root_logger = logging.getLogger('tryp')
+
+
+def tryp_logger(name: str):
+    return tryp_root_logger.getChild(name)
+
+
+def tryp_logging():
+    tryp_root_logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+    if tryp.development:
+        tryp_root_logger.setLevel(logging.DEBUG)
+
+
+logfile = Path.home() / '.python' / 'log'
+
+
+def tryp_file_logging():
+    logfile.parent.mkdir(exist_ok=True)
+    tryp_root_logger.addHandler(logging.FileHandler(str(logfile)))
+
+
+class Logging(object):
+
+    @lazy
+    def log(self):
+        return tryp_logger(self.__class__.__name__)
+
+__all__ = ['tryp_root_logger', 'tryp_logging', 'tryp_file_logging']
