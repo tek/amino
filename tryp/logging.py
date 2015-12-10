cb11cd23b14ad35bb8e87151df634b1195446393
tek <torstenschmits@gmail.com>
2015-12-10 22:25:09 +0100

    add handlers only once

--- tryp/logging.py
+++ tryp/logging.py
@@ -12,19 +12,28 @@ log = tryp_root_logger = logging.getLogger('tryp')
 def tryp_logger(name: str):
     return tryp_root_logger.getChild(name)
 
+_stdout_logging_initialized = False
+
 
 def tryp_stdout_logging():
-    tryp_root_logger.addHandler(logging.StreamHandler(stream=sys.stdout))
-    if tryp.development:
-        tryp_root_logger.setLevel(logging.DEBUG)
+    global _stdout_logging_initialized
+    if not _stdout_logging_initialized:
+        tryp_root_logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+        if tryp.development:
+            tryp_root_logger.setLevel(logging.DEBUG)
+        _stdout_logging_initialized = True
 
 
 logfile = Path.home() / '.python' / 'log'  # type: ignore
+_file_logging_initialized = False
 
 
 def tryp_file_logging():
-    logfile.parent.mkdir(exist_ok=True)
-    tryp_root_logger.addHandler(logging.FileHandler(str(logfile)))
+    global _file_logging_initialized
+    if not _file_logging_initialized:
+        logfile.parent.mkdir(exist_ok=True)
+        tryp_root_logger.addHandler(logging.FileHandler(str(logfile)))
+        _file_logging_initialized = True
 
 
 class Logging(object):
