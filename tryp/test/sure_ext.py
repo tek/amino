49a1482f4d13b3c522da7a9dc35a6d84dadb8846
tek <torstenschmits@gmail.com>
2015-12-15 12:45:23 +0100

    install_assertion_builder function instead of automatic application

--- tryp/test/sure_ext.py
+++ tryp/test/sure_ext.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import sure
 
 from tryp import Maybe, Empty, Just
@@ -17,15 +19,27 @@ class AssBuilder(sure.AssertionBuilder):
         self.obj.should.be.a(Maybe)
         if self.negative:
             msg = "{} contains {}, expected {}"
-            assert(
-                isinstance(self.obj, Empty) or self.obj._get != what,
+            assert isinstance(self.obj, Empty) or self.obj._get != what,\
                 msg.format(self.obj, self.obj._get, what)
-            )
         else:
             self.be.a(Just)
             self.obj._get.should.equal(what)
         return True
 
-sure.AssertionBuilder = AssBuilder
+    def _bool(self, pred, agent, action):
+        no = ('not ' if self.negative else '') + 'to '
+        assert pred(self.obj) ^ self.negative,\
+            'expected {} \'{}\' {} {}'.format(agent, self.obj, no, action)
+        return True
 
-__all__ = ()
+    @sure.assertionproperty
+    def exist(self):
+        err = "can only check existence of Path, not {}"
+        assert isinstance(self.obj, Path), err.format(self.obj)
+        return self._bool(lambda a: a.exists(), "path", "exist")
+
+
+def install_assertion_builder(builder):
+    sure.AssertionBuilder = builder
+
+__all__ = ('install_assertion_builder', 'AssBuilder')
