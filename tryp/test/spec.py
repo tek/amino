bd4bf3afce0cf3b90ee1353e03dec2946f777293
tek <torstenschmits@gmail.com>
2015-12-15 12:46:11 +0100

    install assertion builder in spec base class

--- tryp/test/spec.py
+++ tryp/test/spec.py
@@ -2,7 +2,7 @@ import tek  # type: ignore
 
 import tryp
 from tryp.logging import tryp_stdout_logging
-import tryp.test.sure_ext
+from tryp.test.sure_ext import install_assertion_builder, AssBuilder
 
 
 class Spec(tek.Spec):
@@ -10,6 +10,7 @@ class Spec(tek.Spec):
     def setup(self, *a, **kw):
         tryp.development = True
         tryp_stdout_logging()
+        install_assertion_builder(AssBuilder)
         super(Spec, self).setup(*a, **kw)
 
 __all__ = ('Spec')
