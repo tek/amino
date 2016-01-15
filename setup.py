95387e75c578344bcb73585fa70539e39e06beec
tek <torstenschmits@gmail.com>
2016-01-15 18:54:46 +0100

    2.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages  # type: ignore
 setup(
     name='tryp',
     description='tryp tools',
-    version='2.0.5',
+    version='2.1.0',
     author='Torsten Schmits',
     author_email='torstenschmits@gmail.com',
     license='MIT',
