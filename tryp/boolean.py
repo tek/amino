3214987f784d609e35817bd153f4a7404374d7a5
tek <torstenschmits@gmail.com>
2016-01-19 20:45:04 +0100

    Boolean.{wrap,__str__,__repr__}

--- tryp/boolean.py
+++ tryp/boolean.py
@@ -6,6 +6,10 @@ class Boolean(object):
     def __init__(self, value: bool) -> None:
         self.value = bool(value)
 
+    @staticmethod
+    def wrap(value):
+        return Boolean(value)
+
     def maybe(self, value):
         return Maybe(value) if self else Empty()
 
@@ -17,5 +21,10 @@ class Boolean(object):
 
     def __bool__(self):
         return self.value
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, self.value)
+
+    __repr__ = __str__
 
 __all__ = ['Boolean']
