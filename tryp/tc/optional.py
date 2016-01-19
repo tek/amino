e61157f0d954415e7553887f0e0682bfcec94e76
tek <torstenschmits@gmail.com>
2016-01-19 23:13:11 +0100

    Optional.get_or_else

--- tryp/tc/optional.py
+++ tryp/tc/optional.py
@@ -1,7 +1,7 @@
 import abc
-from typing import TypeVar, Generic, Callable
+from typing import TypeVar, Generic, Callable, Union
 
-from tryp.tc.base import TypeClass, tc_prop
+from tryp.tc.base import TypeClass
 import tryp.maybe
 
 F = TypeVar('F')
@@ -14,5 +14,10 @@ class Optional(Generic[F], TypeClass):
     @abc.abstractmethod
     def to_maybe(self, fa: F) -> 'tryp.maybe.Maybe[B]':
         ...
+
+    def get_or_else(self, fa: F, a: Union[A, Callable[[], A]]):
+        return self.to_maybe(fa).get_or_else(a)
+
+    __or__ = get_or_else
 
 __all__ = ('Optional')
