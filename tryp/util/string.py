9ef32663fe6ce2c291443567b3ce2cc9e6f31c88
tek <torstenschmits@gmail.com>
2016-01-21 23:11:58 +0100

    decode: automatically decode lists, maps, exceptions of bytes

--- tryp/util/string.py
+++ tryp/util/string.py
@@ -1,7 +1,38 @@
 import re
+from functools import singledispatch  # type: ignore
+
 
 def snake_case(name):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-__all__ = ('snake_case')
+
+@singledispatch
+def decode(value):
+    return value
+
+
+@decode.register(bytes)
+def decode_bytes(value):
+    return value.decode()
+
+
+@decode.register(list)
+def decode_list(value):
+    from tryp import List
+    return List.wrap(value).map(decode)
+
+
+@decode.register(dict)
+def decode_dict(value):
+    from tryp import Map
+    return Map.wrap(value)\
+        .keymap(decode)\
+        .valmap(decode)
+
+
+@decode.register(Exception)
+def decode_exc(value):
+    return decode_list(value.args).head | str(value)
+
+__all__ = ('snake_case', 'decode')
