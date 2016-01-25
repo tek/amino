790a3a07ea7f6056ec7d132282b4abe9e96e778a
tek <torstenschmits@gmail.com>
2016-01-25 18:07:28 +0100

    lazy property support for classes with __slots__
    requires using LazyMeta as metaclass

--- tryp/lazy.py
+++ tryp/lazy.py
@@ -1,23 +1,62 @@
-from typing import TypeVar, Generic, Callable, Union, Any
+from typing import TypeVar, Generic, Callable, Union
 import functools
 
 A = TypeVar('A')
+_attr_fmt = '_{}__value'
+
+
+class LazyMeta(type):
+
+    def __new__(mcs, name, bases, dct):
+        lazies = [k for k, v in dct.items() if isinstance(v, lazy)]
+        attrs = tuple(map('_{}__value'.format, lazies))
+        inst = super().__new__(mcs, name, bases, dct)
+        inst.__slots__ = attrs
+        return inst
+
+
+class LazyError(Exception):
+
+    def __init__(self, name, inst):
+        self.name = name
+        self.inst = inst
+
+    def __str__(self):
+        msg = (
+            'class {} with lazy attribute must have a __dict__,  a {}' +
+            ' slot or use LazyMeta as metaclass'
+        )
+        return msg.format(type(self.inst).__name__, self.name)
 
 
 class lazy(Generic[A]):
 
     def __init__(self, func: Callable[..., A]) -> None:
         self.func = func
-        self._attr_name = '_{}__value'.format(self.func.__name__)
+        self._attr_name = _attr_fmt.format(self.func.__name__)
         functools.wraps(self.func)(self)  # type: ignore
 
     def __get__(self, inst, inst_cls) -> Union[A, 'lazy[A]']:
-        if inst is None:
-            return self
-        else:
-            assert hasattr(inst, '__dict__')
-            if not hasattr(inst, self._attr_name):
-                setattr(inst, self._attr_name, self.func(inst))
-            return getattr(inst, self._attr_name)
+        return self if inst is None else self._get(inst, inst_cls)
+
+    def _get(self, inst, inst_cls):
+        self._check(inst)
+        return getattr(inst, self._attr_name)
+
+    def _valid_slot(self, inst):
+        return hasattr(inst, '__slots__') and self._attr_name in inst.__slots__
+
+    def _check(self, inst):
+        if not (hasattr(inst, '__dict__') or self._valid_slot):
+            self._complain()
+        if not hasattr(inst, self._attr_name):
+            object.__setattr__(inst, self._attr_name, self.func(inst))
+
+    def _complain(self, inst):
+        raise LazyError(self._attr_name, inst)
+
+
+class Lazy(metaclass=LazyMeta):
+    pass
 
 __all__ = ['lazy']
--- unit/lazy_spec.py
+++ unit/lazy_spec.py
@@ -1,26 +1,46 @@
 import sure  # NOQA
 from flexmock import flexmock  # NOQA
 
-from tryp.lazy import lazy
+from tryp.lazy import lazy, Lazy
 from tryp.test import Spec
 
 
-class Test(object):
+class A(Lazy):
+    __slots__ = ['a']
     i = 0
 
     @lazy
     def test(self) -> int:
-        Test.i += 1
-        return Test.i
+        A.i += 1
+        return A.i
+
+
+class B(object):
+    i = 0
+
+    @lazy
+    def test(self) -> int:
+        B.i += 1
+        return B.i
 
 
 class Lazy_(Spec):
 
-    def lazy(self):
-        t = Test()
-        Test.i.should.equal(0)
+    def _impl(self, tpe):
+        t = tpe()
+        t2 = tpe()
+        tpe.i.should.equal(0)
+        t.test.should.equal(1)
+        t2.test.should.equal(2)
         t.test.should.equal(1)
         t.test.should.equal(1)
-        Test.i.should.equal(1)
+        t.test.should.equal(1)
+        tpe.i.should.equal(2)
+
+    def slots(self):
+        self._impl(A)
+
+    def dict(self):
+        self._impl(B)
 
 __all__ = ['Lazy_']
