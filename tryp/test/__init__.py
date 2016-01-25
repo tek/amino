9042a411cfefab2f24383083727161f8126a064f
tek <torstenschmits@gmail.com>
2016-01-25 18:13:09 +0100

    move test helpers from tek.test here

--- tryp/test/__init__.py
+++ tryp/test/__init__.py
@@ -1,3 +1,7 @@
-from .spec import Spec, IntegrationSpec  # NOQA
+from .spec import Spec, IntegrationSpec, later  # NOQA  # type: ignore
+from .path import (setup, create_temp_file, temp_file, temp_path, temp_dir,
+                   fixture_path, load_fixture)  # NOPQ
 
-__all__ = ('Spec', 'IntegrationSpec')
+__all__ = ('Spec', 'IntegrationSpec', 'create_temp_file', 'temp_file',
+           'temp_path', 'temp_dir', 'fixture_path', 'load_fixture', 'setup',
+           'later')
--- tryp/test/spec.py
+++ tryp/test/spec.py
@@ -1,20 +1,52 @@
 import os
 import time
+import shutil
+import warnings
+from datetime import datetime
 
-import tek  # type: ignore
+import spec  # type: ignore
 
 import tryp
 from tryp.logging import tryp_stdout_logging
 from tryp.test.sure_ext import install_assertion_builder, AssBuilder
+from tryp.test import path
 
 
-class Spec(tek.Spec):
+def later(ass, timeout=1, intval=0.1):
+    start = datetime.now()
+    ok = False
+    while not ok and (datetime.now() - start).total_seconds() < timeout:
+        try:
+            ass()
+            ok = True
+        except AssertionError:
+            time.sleep(intval)
+    return ass()
 
-    def setup(self, *a, **kw):
+
+class Spec(spec.Spec):
+
+    def __init__(self):
+        self._warnings = True
+
+    def setup(self):
+        if path.__base_dir__:
+            shutil.rmtree(str(path.temp_path()), ignore_errors=True)
+        if self._warnings:
+            warnings.resetwarnings()
         tryp.development = True
         tryp_stdout_logging()
         install_assertion_builder(AssBuilder)
-        super(Spec, self).setup(*a, **kw)
+
+    def teardown(self, *a, **kw):
+        warnings.simplefilter('ignore')
+
+    def _wait_for(self, pred, timeout=5, intval=0.1):
+        start = datetime.now()
+        while (not pred() and
+               (datetime.now() - start).total_seconds() < timeout):
+            time.sleep(intval)
+        pred().should.be.ok
 
     def _wait(self, seconds):
         time.sleep(seconds)
@@ -22,9 +54,9 @@ class Spec(tek.Spec):
 
 class IntegrationSpec(Spec):
 
-    def setup(self, *a, **kw):
+    def setup(self):
         os.environ['TRYP_INTEGRATION'] = '1'
         tryp.integration_test = True
-        super().setup(*a, **kw)
+        super().setup()
 
 __all__ = ('Spec', 'IntegrationSpec')
