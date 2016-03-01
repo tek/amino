52de3ead94d254f2f4438c838e24598ecd5e7ed5
tek <torstenschmits@gmail.com>
2016-03-02 00:11:41 +0100

    Map.bimap

--- tryp/map.py
+++ tryp/map.py
@@ -82,6 +82,9 @@ class Map(Dict[A, B], Generic[A, B]):  # type: ignore
             .flatten
         return Map(filtered)
 
+    def bimap(self, fa: Callable[[A], C], fb: Callable[[B], D]) -> 'Map[C, D]':
+        return self.map(lambda item: (fa(item[0]), fb(item[1])))
+
     @property
     def to_list(self):
         return List.wrap(self.items())
