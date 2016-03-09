a38c7744df065c25038fd806aa60fe4a09efcdd4
tek <torstenschmits@gmail.com>
2016-03-09 15:44:33 +0100

    Traverse.filter{,_not}

--- tryp/tc/traverse.py
+++ tryp/tc/traverse.py
@@ -1,5 +1,5 @@
 import abc
-from typing import TypeVar, Generic
+from typing import TypeVar, Generic, Callable
 
 from tryp.tc.base import TypeClass  # type: ignore
 
@@ -13,5 +13,12 @@ class Traverse(Generic[F], TypeClass):
     @abc.abstractmethod
     def with_index(self, fa: F) -> F:
         ...
+
+    @abc.abstractmethod
+    def filter(self, fa: F, f: Callable[[A], bool]):
+        ...
+
+    def filter_not(self, fa: F, f: Callable[[A], bool]):
+        return self.filter(fa, lambda a: not f(a))
 
 __all__ = ('Traverse',)
