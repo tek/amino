aff0f2887bb272d2cd32bcd344d56558daeb441a
tek <torstenschmits@gmail.com>
2016-03-10 01:09:45 +0100

    make AnonFunc chainable

--- tryp/anon.py
+++ tryp/anon.py
@@ -1,33 +1,45 @@
-class AnonFunc(object):
+class AnonFunc():
 
-    def __init__(self, name, a, kw):
+    def __init__(self, pre: 'AnonFunc', name: str, a, kw) -> None:
+        self.pre = pre
         self.name = name
         self.a = a
         self.kw = kw
 
     def __call__(self, obj):
-        if not hasattr(obj, self.name):
+        pre = self.pre(obj)
+        if not hasattr(pre, self.name):
             raise AttributeError(
                 '{!r} has no method \'{}\''.format(obj, self.name))
-        return getattr(obj, self.name)(*self.a, **self.kw)
+        return getattr(pre, self.name)(*self.a, **self.kw)
 
     def __repr__(self):
         return '{}({})'.format(self.__class__.__name__, self.name)
 
+    def __getattr__(self, name):
+        return MethodRef(self, name)
 
-class MethodRef(object):
 
-    def __init__(self, name: str) -> None:
+class MethodRef():
+
+    def __init__(self, pre: AnonFunc, name: str) -> None:
+        self.pre = pre
         self.name = name
 
     def __call__(self, *a, **kw):
-        return AnonFunc(self.name, a, kw)
+        return AnonFunc(self.pre, self.name, a, kw)
 
 
-class MethodLambda(object):
+class IdAnonFunc():
+
+    def __call__(self, obj):
+        return obj
+
+
+class MethodLambda():
 
     def __getattr__(self, name):
-        return MethodRef(name)
+        return MethodRef(IdAnonFunc(), name)
 
 __ = MethodLambda()
 
--- unit/anon_spec.py
+++ unit/anon_spec.py
@@ -2,22 +2,35 @@ import sure  # NOQA
 from flexmock import flexmock  # NOQA
 
 from tryp import __
-from tryp.test import Spec
+from tryp.test import Spec  # type: ignore
 
 
-class Anon_(Spec):
+class Inner(object):
 
-    def setup(self, *a, **kw):
-        super().setup(*a, **kw)
+    def __init__(self, z) -> None:
+        self.z = z
 
-    def anon_call(self):
-        class Adder(object):
-            def add(self, a, b):
-                return a + b
-        adder = Adder()
+    def wrap(self, z):
+        return Inner(self.z + z)
+
+    def add(self, a, b):
+        return self.z + a + b
+
+
+class Outer(object):
+
+    def inner(self, z):
+        return Inner(z)
+
+
+class AnonSpec(Spec):
+
+    def nested(self):
+        z = 5
         a = 3
         b = 4
-        f = __.add(a, b)
-        f(adder).should.equal(7)
+        o = Outer()
+        f = __.inner(z).wrap(z).add(a, b)
+        f(o).should.equal(2 * z + a + b)
 
-__all__ = ('Anon_',)
+__all__ = ('AnonSpec',)
