dc99fc64a699b778b788bf47df3fa4b02c8864bf
tek <torstenschmits@gmail.com>
2016-03-10 01:24:06 +0100

    enable attribute getting from AnonFuncs

--- tryp/anon.py
+++ tryp/anon.py
@@ -1,26 +1,38 @@
-class AnonFunc():
+class AnonGetter:
 
-    def __init__(self, pre: 'AnonFunc', name: str, a, kw) -> None:
+    def __init__(self, pre: 'AnonFunc', name: str) -> None:
         self.pre = pre
         self.name = name
-        self.a = a
-        self.kw = kw
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, self.name)
 
     def __call__(self, obj):
         pre = self.pre(obj)
         if not hasattr(pre, self.name):
             raise AttributeError(
                 '{!r} has no method \'{}\''.format(obj, self.name))
-        return getattr(pre, self.name)(*self.a, **self.kw)
+        return self._dispatch(getattr(pre, self.name))
 
-    def __repr__(self):
-        return '{}({})'.format(self.__class__.__name__, self.name)
+    def _dispatch(self, obj):
+        return obj
+
+
+class AnonFunc(AnonGetter):
+
+    def __init__(self, pre: 'AnonFunc', name: str, a, kw) -> None:
+        super().__init__(pre, name)
+        self.a = a
+        self.kw = kw
+
+    def _dispatch(self, obj):
+        return obj(*self.a, **self.kw)
 
     def __getattr__(self, name):
         return MethodRef(self, name)
 
 
-class MethodRef():
+class MethodRef:
 
     def __init__(self, pre: AnonFunc, name: str) -> None:
         self.pre = pre
@@ -29,14 +41,18 @@ class MethodRef():
     def __call__(self, *a, **kw):
         return AnonFunc(self.pre, self.name, a, kw)
 
+    def __getattr__(self, name):
+        pre = AnonGetter(self.pre, self.name)
+        return MethodRef(pre, name)
 
-class IdAnonFunc():
+
+class IdAnonFunc:
 
     def __call__(self, obj):
         return obj
 
 
-class MethodLambda():
+class MethodLambda:
 
     def __getattr__(self, name):
         return MethodRef(IdAnonFunc(), name)
--- unit/anon_spec.py
+++ unit/anon_spec.py
@@ -1,23 +1,21 @@
-import sure  # NOQA
-from flexmock import flexmock  # NOQA
-
 from tryp import __
 from tryp.test import Spec  # type: ignore
 
 
-class Inner(object):
+class Inner:
 
     def __init__(self, z) -> None:
         self.z = z
 
-    def wrap(self, z):
-        return Inner(self.z + z)
+    @property
+    def wrap(self):
+        return Inner(self.z * 2)
 
     def add(self, a, b):
         return self.z + a + b
 
 
-class Outer(object):
+class Outer:
 
     def inner(self, z):
         return Inner(z)
@@ -30,7 +28,7 @@ class AnonSpec(Spec):
         a = 3
         b = 4
         o = Outer()
-        f = __.inner(z).wrap(z).add(a, b)
+        f = __.inner(z).wrap.add(a, b)
         f(o).should.equal(2 * z + a + b)
 
 __all__ = ('AnonSpec',)
