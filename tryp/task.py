ea461fa1859ba312fd689963cbca0391117bcaf5
tek <torstenschmits@gmail.com>
2016-03-20 22:59:33 +0100

    Task Monad

--- tryp/task.py
+++ tryp/task.py
@@ -1,11 +1,23 @@
 from typing import Callable, TypeVar, Generic
 
 from tryp import Either, Right, Left
+from tryp.tc.monad import Monad
+from tryp.tc.base import ImplicitInstances, Implicits
+from tryp.lazy import lazy
 
 A = TypeVar('A')
+B = TypeVar('B')
 
 
-class Task(Generic[A]):
+class TaskInstances(ImplicitInstances):
+
+    @lazy
+    def _instances(self):
+        from tryp.map import Map
+        return Map({Monad: TaskMonad()})
+
+
+class Task(Generic[A], Implicits, implicits=True):
 
     @staticmethod
     def call(f: Callable[..., A], *a, **kw):
@@ -23,5 +35,14 @@ class Task(Generic[A]):
 
 def Try(f: Callable[..., A], *a, **kw) -> Either[Exception, A]:
     return Task.call(f, *a, **kw).unsafe_perform_sync()
+
+
+class TaskMonad(Monad):
+
+    def pure(self, a: A):
+        return Task(lambda: a)
+
+    def flat_map(self, fa: Task[A], f: Callable[[A], Task[B]]) -> Task[B]:
+        return Task(lambda: f(fa.run()).run())
 
 __all__ = ('Task', 'Try')
