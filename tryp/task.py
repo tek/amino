fab5049ef3a9b9d7cd884fd11473237d5d2a5714
tek <torstenschmits@gmail.com>
2016-07-17 01:42:46 +0200

    Task constructors

--- tryp/task.py
+++ tryp/task.py
@@ -1,6 +1,6 @@
-from typing import Callable, TypeVar, Generic
+from typing import Callable, TypeVar, Generic, Any
 
-from tryp import Either, Right, Left
+from tryp import Either, Right, Left, Maybe
 from tryp.tc.monad import Monad
 from tryp.tc.base import ImplicitInstances, Implicits
 from tryp.lazy import lazy
@@ -22,6 +22,24 @@ class Task(Generic[A], Implicits, implicits=True):
     @staticmethod
     def call(f: Callable[..., A], *a, **kw):
         return Task(lambda: f(*a, **kw))
+
+    @staticmethod
+    def now(a: A) -> 'Task[A]':
+        return Task(lambda: a)
+
+    @staticmethod
+    def failed(err: str) -> 'Task[A]':
+        def fail():
+            raise Exception(err)
+        return Task(fail)
+
+    @staticmethod
+    def from_either(a: Either[Any, A]) -> 'Task[A]':
+        return a.cata(Task.failed, Task.now)
+
+    @staticmethod
+    def from_maybe(a: Maybe[A], error: str) -> 'Task[A]':
+        return a / Task.now | Task.failed(error)
 
     def __init__(self, f: Callable[[], A]) -> None:
         self.run = f
