8ae759520a80feb5b72e417631ae5a6747ee111f
tek <torstenschmits@gmail.com>
2016-08-12 00:58:43 +0200

    remove debug spec

--- unit/task_spec.py
+++ unit/task_spec.py
@@ -10,16 +10,6 @@ class TaskSpec(Spec):
         t = List(Task.now(1), Task.now(2), Task(f)).sequence(Task)
         t.unsafe_perform_sync().should.equal(Right(List(1, 2, 3)))
 
-    def traceback(self):
-        class A:
-            def rais(self):
-                raise Exception('here')
-        def fm(a):
-            return Task.now(a) / __.rais()
-        t = Task.now(A()) // fm
-        e = t.unsafe_perform_sync()
-        print(e)
-
     def zip(self):
         t = Task.now(1) & Task.now(2)
         t.unsafe_perform_sync().should.equal(Right((1, 2)))
