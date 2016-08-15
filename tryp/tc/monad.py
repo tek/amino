90956e0902bbc6c90f82a1afe722b3d17a9723fd
tek <torstenschmits@gmail.com>
2016-08-15 15:10:25 +0200

    make depth or types specification optional for `Monad.effs`

--- tryp/tc/monad.py
+++ tryp/tc/monad.py
@@ -2,8 +2,7 @@ from typing import TypeVar, Callable
 
 from tryp.tc.flat_map import FlatMap
 from tryp.tc.applicative import Applicative
-from tryp.tc.base import tc_prop
-from tryp import List
+from tryp import List, L, _
 
 F = TypeVar('F')
 A = TypeVar('A')
@@ -15,12 +14,20 @@ class Monad(FlatMap, Applicative):
     def map(self, fa: F, f: Callable[[A], B]) -> F:  # type: ignore
         return self.flat_map(fa, lambda a: self.pure(f(a)))
 
-    def eff(self, fa: F, tpe: type):
+    def eff(self, fa: F, tpe: type=None):
         from tryp.eff import Eff
-        return Eff(fa)
+        tpes = List() if tpe is None else List(tpe)
+        return Eff(fa, tpes, 1)
 
-    def effs(self, fa: F, depth: int, *types):
+    def effs(self, fa: F, *args):
         from tryp.eff import Eff
-        return Eff(fa, List.wrap(types), depth=depth)
+        types = List.wrap(args)
+        c = L(Eff)(fa, _, depth=_)
+        with_depth = lambda d, t: c(t, depth=d)
+        types_only = lambda: c(types, depth=len(types))
+        def try_depth(h, t):
+            return (with_depth(int(h), t) if isinstance(h, int) else
+                    types_only())
+        return types.detach_head.map2(try_depth) | types_only
 
 __all__ = ('Monad',)
