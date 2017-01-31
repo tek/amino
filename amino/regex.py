9040cc7025488bda17d351161e649de4e6b755bc
Torsten Schmits <torstenschmits@gmail.com>
2017-01-31 23:00:13 +0100

    `Match.all_groups`

--- amino/regex.py
+++ amino/regex.py
@@ -36,29 +36,42 @@ class Regex:
 
 class Match:
 
-    def __init__(self, regex: Regex, internal, data: str) -> None:
+    def __init__(self, regex: Regex, internal: Any, data: str) -> None:
         self.regex = regex
         self.internal = internal
+        self.data = data
 
     @property
-    def group_map(self):
+    def group_map(self) -> Map[str, str]:
         return Map(self.internal.groupdict())
 
     m = group_map
 
-    def group(self, id):
-        return self.group_map.get(id)
+    def group(self, id: str) -> Either[str, str]:
+        return (
+            self.group_map
+            .get(id)
+            .to_either('no group `{}` in {}'.format(id, self))
+        )
 
     g = group
 
     @property
-    def groups(self):
+    def groups(self) -> List[str]:
         return List.wrap(self.internal.groups())
 
     l = groups
 
+    def all_groups(self, *ids: str) -> Either[str, List[str]]:
+        return (self.group_map.get_all(*ids)
+                .to_either('not all groups `{}` in {}'.format(ids, self)))
+
     @property
-    def match(self):
+    def match(self) -> str:
         return self.internal.group(0)
+
+    def __str__(self) -> str:
+        return 'Match({}, {}, {})'.format(self.regex, self.data,
+                                          self.group_map)
 
 __all__ = ('Regex',)
