305ee38c313dd8175d95fde323ce5e800d20bdb3
Torsten Schmits <torstenschmits@gmail.com>
2017-02-05 16:36:12 +0100

    automatic tc instances variant

--- amino/tc/base.py
+++ amino/tc/base.py
@@ -1,7 +1,6 @@
 import importlib
 import abc
-from typing import Dict, Callable
-from typing import GenericMeta  # type: ignore
+from typing import Dict, Tuple, Any, Callable, Union, Optional, GenericMeta
 from functools import partial, wraps
 
 from amino.util.string import snake_case
@@ -13,10 +12,10 @@ from amino.logging import amino_root_logger
 
 class TypeClassMeta(GenericMeta):
 
-    def __getitem__(self, tpe: type):
+    def __getitem__(self, tpe: type) -> 'TypeClass':
         return Instances.lookup(self, tpe)
 
-    def exists_instance(self, tpe: type):
+    def exists_instance(self, tpe: type) -> bool:
         try:
             self[tpe]
         except ImplicitNotFound:
@@ -27,20 +26,20 @@ class TypeClassMeta(GenericMeta):
     exists = exists_instance
 
 
-class TypeClass(object, metaclass=TypeClassMeta):
+class TypeClass(metaclass=TypeClassMeta):
     pass
 
 
-class TypeClasses(object):
+class TypeClasses:
 
     @property
-    def instances(self):
+    def instances(self) -> Dict[type, TypeClass]:
         pass
 
 
 class ImplicitInstancesNotFound(Exception):
 
-    def __init__(self, mod, cls, name):
+    def __init__(self, mod: str, cls: str, name: str) -> None:
         msg = 'invalid implicit class path {}.{} for {}'.format(mod, cls, name)
         super().__init__(msg)
 
@@ -54,131 +53,16 @@ operators = (
 )
 
 
-class ImplicitsMeta(GenericMeta):
-
-    def _infer_implicits(name):
-        snake = snake_case(name)
-        return 'amino.instances.{}'.format(snake), '{}Instances'.format(name)
-
-    def _mk_operator(name):
-        def dispatch(self, other):
-            return self._operator(name, other)
-        return dispatch
-
-    def _attach_operators(inst):
-        for op in operators:
-            setattr(inst, op, ImplicitsMeta._mk_operator(op))
-
-    def __new__(cls, name, bases, namespace, imp_mod=None, imp_cls=None,
-                implicits=False, **kw):
-        inst = super().__new__(cls, name, bases, namespace, **kw)
-        if not implicits:
-            return inst
-        else:
-            if imp_mod is None or imp_cls is None:
-                imp_mod, imp_cls = ImplicitsMeta._infer_implicits(name)
-            inst.imp_mod = imp_mod
-            inst.imp_cls = imp_cls
-            inst.name = name
-            inst._implicits_instance = None
-            ImplicitsMeta._attach_operators(inst)
-            Instances.add(name, inst)
-            return inst
-
-    @property
-    def _implicits(self):
-        m = self.imp_mod
-        c = self.imp_cls
-        err = ImplicitInstancesNotFound(m, c, self.name)
-        try:
-            mod = importlib.import_module(m)
-        except ImportError:
-            raise err
-        else:
-            if hasattr(mod, c):
-                instances = getattr(mod, c)()
-                return instances
-            else:
-                raise err
-
-    @property
-    def implicits(self):
-        if self._implicits_instance is None:
-            self._implicits_instance = self._implicits.instances
-        return self._implicits_instance
-
-    __copy__ = None
-
-
-def tc_prop(f):
-    f._tc_prop = True
-    return f
-
-
-class Implicits(metaclass=ImplicitsMeta):
-    permanent = True
-
-    def _lookup_implicit_attr(self, name):
-        return next((getattr(inst, name)
-                     for inst in type(self).implicits.v
-                     if hasattr(inst, name)), None)
-
-    def _bound_implicit_attr(self, name):
-        f = self._lookup_implicit_attr(name)
-        if f is not None:
-            if hasattr(f, '_tc_prop'):
-                return f(self)
-            else:
-                return partial(f, self)
-
-    def _set_implicit_attr(self, name):
-        f = self._lookup_implicit_attr(name)
-        if f is not None:
-            @wraps(f)
-            def wrap(self, *a, **kw):
-                return f(self, *a, **kw)
-            g = property(wrap) if hasattr(f, '_tc_prop') else wrap
-            setattr(type(self), name, g)
-            return getattr(self, name)
-
-    def __getattr__(self, name):
-        imp = (self._set_implicit_attr(name) if Implicits.permanent else
-               self._bound_implicit_attr(name))
-        if imp is None:
-            err = '\'{}\' has no attribute \'{}\''.format(self, name)
-            raise AttributeError(err)
-        else:
-            return imp
-
-    def _operator(self, name, other):
-        op = (self._set_implicit_attr(name) if Implicits.permanent else
-              self._bound_implicit_attr(name))
-        if op is None:
-            err = '\'{}\' has no implicit operator \'{}\''.format(self, name)
-            raise TypeError(err)
-        else:
-            return op(other)
-
-    @property
-    def dbg(self):
-        amino_root_logger.verbose(self)
-        return self
-
-    @property
-    def dbgr(self):
-        v = self.log.verbose
-        v(repr(self))
-        return self
-
-
-class ImplicitInstances(object, metaclass=abc.ABCMeta):
+class ImplicitInstances(metaclass=abc.ABCMeta):
 
     @lazy
-    def instances(self):
-        return (TC.instances ** self._instances **
-                self._override_instances)
+    def instances(self) -> Dict[type, TypeClass]:
+        return (
+            TC.instances ** self._instances **  # type: ignore
+            self._override_instances
+        )
 
-    @abc.abstractproperty  # type: ignore
+    @abc.abstractproperty
     def _instances(self) -> Dict[type, TypeClass]:
         ...
 
@@ -188,10 +72,174 @@ class ImplicitInstances(object, metaclass=abc.ABCMeta):
         return Map()
 
 
+def lookup_implicit_instances(name: str, m: str, c: str) -> ImplicitInstances:
+    err = ImplicitInstancesNotFound(m, c, name)
+    try:
+        mod = importlib.import_module(m)
+    except ImportError:
+        raise err
+    else:
+        if hasattr(mod, c):
+            instances = getattr(mod, c)()
+            return instances
+        else:
+            raise err
+
+
+class InstancesMetadata:
+
+    def __init__(self, name: str, mod: str, cls: str) -> None:
+        self.name = name
+        self.mod = mod
+        self.cls = cls
+        self._instances = None  # type: Optional[ImplicitInstances]
+
+    def __str__(self) -> str:
+        return '{}({}, {}, {})'.format(self.__class__.__name__, self.name,
+                                       self.mod, self.cls)
+
+    @property
+    def instances(self) -> Dict[str, TypeClass]:
+        if self._instances is None:
+            self._instances = lookup_implicit_instances(self.name, self.mod,
+                                                        self.cls)
+        return self._instances.instances
+
+
+def _infer_implicits(name: str) -> Tuple[str, str]:
+    snake = snake_case(name)
+    return 'amino.instances.{}'.format(snake), '{}Instances'.format(name)
+
+
+class ImplicitsMeta(GenericMeta):
+
+    @staticmethod
+    def _mk_operator(name: str) -> Callable:
+        def dispatch(self: Any, other: Any) -> Any:
+            return self._operator(name, other)
+        return dispatch
+
+    def _attach_operators(inst) -> None:
+        for op in operators:
+            setattr(inst, op, ImplicitsMeta._mk_operator(op))
+
+    def __new__(cls: type, name: str, bases: tuple, namespace: dict,
+                imp_mod: str=None, imp_cls: str=None, implicits: bool=False,
+                **kw: dict) -> type:
+        inst = super().__new__(cls, name, bases, namespace,  # type: ignore
+                               **kw)
+        if not implicits:
+            inst._instances_meta = None
+            return inst
+        else:
+            if imp_mod is None or imp_cls is None:
+                imp_mod, imp_cls = _infer_implicits(name)
+            inst.name = name
+            ImplicitsMeta._attach_operators(inst)
+            meta = InstancesMetadata(name, imp_mod, imp_cls)
+            inst.imp_mod = imp_mod
+            inst.imp_cls = imp_cls
+            inst.instances_meta = meta
+            Instances.add(meta)
+            return inst
+
+    __copy__ = None
+
+
+def tc_prop(f: Callable) -> Callable:
+    f._tc_prop = True  # type: ignore
+    return f
+
+
+class Implicits(metaclass=ImplicitsMeta):
+    permanent = True
+
+    def _lookup_implicit_attr(self, name: str) -> Optional[Callable]:
+        meta = type(self).instances_meta  # type: ignore
+        if meta is not None:
+            return next((getattr(inst, name)
+                        for inst in meta.instances.v
+                        if hasattr(inst, name)), None)
+
+    def _bound_implicit_attr(self, name: str
+                             ) -> Union[None, Callable, partial]:
+        f = self._lookup_implicit_attr(name)
+        if f is not None:
+            if hasattr(f, '_tc_prop'):
+                return f(self)
+            else:
+                return partial(f, self)
+        return None
+
+    def _set_implicit_attr(self, name: str) -> Any:
+        f = self._lookup_implicit_attr(name)
+        if f is not None:
+            @wraps(f)
+            def wrap(self: Any, *a: Any, **kw: Any) -> Any:
+                return f(self, *a, **kw)  # type: ignore
+            g = property(wrap) if hasattr(f, '_tc_prop') else wrap
+            setattr(type(self), name, g)
+            return getattr(self, name)
+
+    def __getattr__(self, name: str) -> Callable:
+        imp = (self._set_implicit_attr(name) if Implicits.permanent else
+               self._bound_implicit_attr(name))
+        if imp is None:
+            err = '\'{}\' has no attribute \'{}\''.format(self, name)
+            raise AttributeError(err)
+        else:
+            return imp
+
+    def _operator(self, name: str, other: Any) -> Any:
+        op = (self._set_implicit_attr(name) if Implicits.permanent else
+              self._bound_implicit_attr(name))
+        if op is None:
+            err = '\'{}\' has no implicit operator \'{}\''.format(self, name)
+            raise TypeError(err)
+        else:
+            return op(other)
+
+    @property
+    def dbg(self) -> Any:
+        amino_root_logger.verbose(self)
+        return self
+
+    @property
+    def dbgr(self) -> Any:
+        v = self.log.verbose  # type: ignore
+        v(repr(self))
+        return self
+
+
+class TypeAttrMissing(Exception):
+
+    def __init__(self, name: str) -> None:
+        msg = 'Instances class `{}` has no attribute `tpe`'.format(name)
+        super().__init__(msg)
+
+
+class AutoImplicitInstancesMeta(abc.ABCMeta):
+
+    def __new__(cls: type, name: str, bases: tuple, namespace: dict) -> type:
+        inst = super().__new__(cls, name, bases, namespace)
+        if name != 'AutoImplicitInstances':
+            imp_mod = inst.__module__
+            if not hasattr(inst, 'tpe'):
+                raise TypeAttrMissing(name)
+            tpe_name = getattr(inst, 'tpe').__name__
+            Instances.add(InstancesMetadata(tpe_name, imp_mod, name))
+        return inst
+
+
+class AutoImplicitInstances(ImplicitInstances,
+                            metaclass=AutoImplicitInstancesMeta):
+    pass
+
+
 class GlobalTypeClasses(TypeClasses):
 
     @property
-    def instances(self):
+    def instances(self) -> Dict[type, TypeClass]:
         from amino.map import Map
         from amino.tc.tap import Tap
         return Map({Show: Show(), Tap: Tap()})
@@ -202,20 +250,20 @@ TC = GlobalTypeClasses()
 
 class ImplicitNotFound(Exception):
 
-    def __init__(self, tc, a):
+    def __init__(self, tc: type, a: type) -> None:
         msg = 'no type class found for {}[{}]'.format(tc, a)
         super().__init__(msg)
 
 
-class AllInstances(object):
+class AllInstances:
 
-    def __init__(self):
-        self._instances = dict()
+    def __init__(self) -> None:
+        self._instances = dict()  # type: Dict[str, InstancesMetadata]
 
-    def add(self, name, inst: Callable[[], ImplicitInstances]):
-        self._instances[name] = inst
+    def add(self, data: InstancesMetadata) -> None:
+        self._instances[data.name] = data
 
-    def lookup(self, TC, G):
+    def lookup(self, TC: type, G: type) -> TypeClass:
         ''' Find an instance of the type class `TC` for type `G`.
         Iterates `G`'s parent classes, looking up instances for each,
         checking whether the instance is a subclass of the target type
@@ -224,17 +272,23 @@ class AllInstances(object):
         from amino.lazy_list import LazyList
         from amino.anon import _, L
         match = L(self._lookup_type)(TC, _)
-        result = LazyList(map(match, G.__mro__))\
-            .find(_.is_just)\
-            .join\
+        result = (
+            LazyList(map(match, G.__mro__))
+            .find(_.is_just)
+            .join
             .get_or_raise(ImplicitNotFound(TC, G))
+        )
         return result[1]
 
-    def _lookup_type(self, TC, G):
+    def _lookup_type(self, TC: type, G: type
+                     ) -> 'amino.maybe.Maybe[TypeClass]':
         from amino.maybe import Empty
         if G.__name__ in self._instances:
             match = lambda I: isinstance(I, TC)
-            return self._instances[G.__name__].implicits.find(match)
+            return (
+                self._instances[G.__name__].instances  # type: ignore
+                .find(match)
+            )
         else:
             return Empty()
 
--- unit/implicit_spec.py
+++ unit/implicit_spec.py
@@ -1,11 +1,68 @@
-from amino import Just
+from typing import Callable
+
+from amino import Just, Map, List
 from amino.test import Spec
 from amino.instances.maybe import MaybeMonad
+from amino.tc.base import (ImplicitInstances, Instances, InstancesMetadata,
+                           AutoImplicitInstances, TypeClass)
+from amino.tc.monoid import Monoid
+from amino.lazy import lazy
+from amino.instances.list import ListMonoid
+from amino.tc.functor import Functor
+
+
+class A:
+
+    def __init__(self, a: int) -> None:
+        self.a = a
+
+
+class AMonoid(Monoid):
+
+    @property
+    def empty(self) -> A:
+        return A(0)
+
+    def combine(self, l: A, r: A) -> A:
+        return A(l.a + r.a)
+
+
+class AInstances(ImplicitInstances):
+
+    @lazy
+    def _instances(self) -> Map[str, TypeClass]:
+        return Map({Monoid: AMonoid()})
+
+
+Instances.add(InstancesMetadata('A', 'unit.implicit_spec', 'AInstances'))
+
+
+class B:
+    pass
+
+
+class BFunctor(Functor):
+
+    def map(self, fa: B, f: Callable) -> B:
+        return fa
+
+
+class BInstances(AutoImplicitInstances):
+    tpe = B
+
+    @lazy
+    def _instances(self) -> Map[str, TypeClass]:
+        return Map({Functor: BFunctor()})
 
 
 class ImplicitSpec(Spec):
 
-    def set_attr(self):
+    def set_attr(self) -> None:
         Just(1).map.__wrapped__.__self__.should.be.a(MaybeMonad)
+
+    def manual(self) -> None:
+        Monoid[A].should.be.a(AMonoid)
+        Monoid[List].should.be.a(ListMonoid)
+        Functor[B].should.be.a(BFunctor)
 
 __all__ = ('ImplicitSpec',)
