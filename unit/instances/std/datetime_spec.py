f0d944284e9ce675fd6ccd6b97cee5d42b88cd88
Torsten Schmits <torstenschmits@gmail.com>
2017-02-16 22:08:20 +0100

    `Monoid` for `timedelta`

---
+++ unit/instances/std/datetime_spec.py
@@ -0,0 +1,24 @@
+from datetime import timedelta
+
+from amino.test import Spec
+from amino.tc.monoid import Monoid
+from amino.instances.std.datetime import TimedeltaInstances  # NOQA
+from amino import List
+
+
+class TimedeltaSpec(Spec):
+
+    def empty(self) -> None:
+        Monoid[timedelta].empty.should.equal(timedelta(seconds=0))
+
+    def combine(self) -> None:
+        a = timedelta(seconds=5)
+        b = timedelta(seconds=12)
+        Monoid[timedelta].combine(a, b).should.equal(a + b)
+
+    def fold(self) -> None:
+        secs = List(4, 23, 45, 71)
+        result = secs.map(lambda a: timedelta(seconds=a)).fold(timedelta)
+        result.should.equal(timedelta(seconds=sum(secs)))
+
+__all__ = ('TimedeltaSpec',)
