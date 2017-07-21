532d0ec79b971331096dd7f5a546bb84081cba42
Torsten Schmits <torstenschmits@gmail.com>
2017-07-21 19:50:34 +0200

    tweak `Eval`

--- amino/eval.py
+++ amino/eval.py
@@ -3,7 +3,6 @@ from typing import Generic, TypeVar, Callable, Tuple, Any, Union
 
 from fn.recur import tco
 
-from amino.lazy import lazy
 from amino.tc.base import Implicits
 from amino.tc.monad import Monad
 from amino import List
@@ -13,6 +12,18 @@ B = TypeVar('B')
 
 
 class Eval(Generic[A], Implicits, implicits=True, auto=True):
+
+    @staticmethod
+    def now(a: A) -> 'Eval[A]':
+        return Now(a)
+
+    @staticmethod
+    def later(f: Callable[..., A], *a: Any, **kw: Any) -> 'Eval[A]':
+        return Later(lambda: f(*a))
+
+    @staticmethod
+    def always(f: Callable[..., A], *a: Any, **kw: Any) -> 'Eval[A]':
+        return Always(lambda: f(*a))
 
     @abc.abstractmethod
     def _value(self) -> A:
@@ -39,22 +50,30 @@ class Now(Generic[A], Eval[A]):
     def memoize(self) -> Eval[A]:
         return self
 
+    def __str__(self) -> str:
+        return f'Now({self.strict})'
+
 
 class Later(Generic[A], Eval[A]):
 
     def __init__(self, f: Callable[[], A]) -> None:
         self.f = f
+        self._memo = None  # type: Union[A, None]
 
-    @lazy
     def _memoized(self) -> A:
-        return self.f()
+        if self._memo is None:
+            self._memo = self.f()
+        return self._memo
 
     def _value(self) -> A:
-        return self._memoized
+        return self._memoized()
 
     @property
     def memoize(self) -> Eval[A]:
         return self
+
+    def __str__(self) -> str:
+        return f'Later({self.f})'
 
 
 class Always(Generic[A], Eval[A]):
@@ -68,6 +87,9 @@ class Always(Generic[A], Eval[A]):
     @property
     def memoize(self) -> Eval[A]:
         return Later(self.f)
+
+    def __str__(self) -> str:
+        return f'Always({self.f})'
 
 
 class Call(Generic[A], Eval[A]):
@@ -84,32 +106,42 @@ class Call(Generic[A], Eval[A]):
             if isinstance(e, Call):
                 return True, (e.thunk(),)
             elif isinstance(e, Compute):
-                return False, (Compute(e.start, loop1),)
+                return False, (Compute(e.start, loop1, e.start_str),)
             else:
                 return False, e
         return loop(ev)
 
+    @property
+    def memoize(self) -> Eval[A]:
+        return Later(lambda: self._value())
+
+    def _value(self) -> A:
+        return Call._loop(self).value
+
+    def __str__(self) -> str:
+        return f'Call({self.thunk})'
+
 
 class Compute(Generic[A, B], Eval[A]):
 
-    def __init__(self, start: Callable[[], Eval[B]], run: Callable[[B], Eval[A]]) -> None:
+    def __init__(self, start: Callable[[], Eval[B]], run: Callable[[B], Eval[A]], start_str: str) -> None:
         self.start = start
         self.run = run
+        self.start_str = start_str
 
     def _value(self) -> A:
         C = Callable[[Any], Eval[Any]]
-        R = Tuple[bool, Union[Tuple[Eval[Any], List[C]], Any]]
-        def loop_compute(c: Compute[Any, Any], fs: List[C]) -> R:
+        def loop_compute(c: Compute[Any, Any], fs: List[C]) -> Tuple[bool, Union[Tuple[Eval[Any], List[C]], Any]]:
             cc = c.start()
             return (
                 (True, (cc.start(), fs.cons(c.run).cons(cc.run)))
                 if isinstance(cc, Compute) else
                 (True, (c.run(cc._value()), fs))
             )
-        def loop_other(e: Eval[Any], fs: List[C]) -> R:
+        def loop_other(e: Eval[Any], fs: List[C]) -> Tuple[bool, Union[Tuple[Eval[Any], List[C]], Any]]:
             return fs.detach_head.map2(lambda fh, ft: (True, (fh(e._value()), ft))) | (False, e._value())
         @tco
-        def loop(e: Eval[Any], fs: List[C]) -> R:
+        def loop(e: Eval[Any], fs: List[C]) -> Tuple[bool, Union[Tuple[Eval[Any], List[C]], Any]]:
             return (
                 loop_compute(e, fs)
                 if isinstance(e, Compute) else
@@ -121,6 +153,9 @@ class Compute(Generic[A, B], Eval[A]):
     def memoize(self) -> Eval[A]:
         return Later(self)
 
+    def __str__(self) -> str:
+        return f'Compute({self.start_str})'
+
 
 class EvalMonad(Monad, tpe=Eval):
 
@@ -129,14 +164,18 @@ class EvalMonad(Monad, tpe=Eval):
 
     def flat_map(self, fa: Eval[A], f: Callable[[A], Eval[B]]) -> Eval[B]:
         def f1(s: A) -> Eval[B]:
-            return Compute(lambda: fa.run(s), f)
-        start, run = (
-            (fa.start, f1)
+            return Compute(lambda: fa.run(s), f, '')
+        start, run, start_str = (
+            (fa.start, f1, fa.start_str)
             if isinstance(fa, Compute) else
-            (fa.thunk, f)
+            (fa.thunk, f, fa.thunk_str)
             if isinstance(fa, Call) else
-            (lambda: fa, f)
+            (lambda: fa, f, str(fa))
         )
-        return Compute(start, run)
+        return Compute(start, run, start_str)
 
-__all__ = ('Eval',)
+
+def defer_eval(f: Callable[[], Eval[A]]) -> Eval[A]:
+    return Call(f)
+
+__all__ = ('Eval', 'defer_eval')
