8afdc8a6663d97281327b91c730d9eb97964ac1e
Torsten Schmits <torstenschmits@gmail.com>
2017-07-21 19:49:41 +0200

    export `Eval` and `Regex`

--- amino/__init__.py
+++ amino/__init__.py
@@ -13,13 +13,14 @@ from amino.env_vars import env
 from amino.task import Try, Task
 from amino.logging import Logger, log
 from amino.eff import Eff
+from amino.eval import Eval
+from amino.regex import Regex
 
 development = 'AMINO_DEVELOPMENT' in env
 integration_test = 'AMINO_INTEGRATION' in env
 
 Path = pathlib.Path
 
-__all__ = ('Maybe', 'Just', 'Empty', 'may', 'List', 'Map', '_', 'Future',
-           'Boolean', 'development', 'flat_may', 'curried', '__', 'F', 'Left',
-           'Right', 'Either', 'env', 'Try', 'LazyList', 'Logger', 'log', 'I',
-           'L', 'Eff', 'Task')
+__all__ = ('Maybe', 'Just', 'Empty', 'may', 'List', 'Map', '_', 'Future', 'Boolean', 'development', 'flat_may',
+           'curried', '__', 'F', 'Left', 'Right', 'Either', 'env', 'Try', 'LazyList', 'Logger', 'log', 'I', 'L', 'Eff',
+           'Task', 'Eval', 'Regex')
