29163482dce85732cd4594216d4a846ebae7a1bc
Torsten Schmits <torstenschmits@gmail.com>
2017-08-20 01:03:32 +0200

    10.6.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version_parts = (10, 6, 0)
+version_parts = (10, 6, 1)
 version = '.'.join(map(str, version_parts))
 
 gh_lenses = 'git+https://github.com/ingolemo/python-lenses.git#egg=lenses'
