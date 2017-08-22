4f1d734eb4c2c5a8494c38c9075ce65d1be0d460
Torsten Schmits <torstenschmits@gmail.com>
2017-08-22 02:29:02 +0200

    types for `EnvVars`

--- amino/env_vars.py
+++ amino/env_vars.py
@@ -1,6 +1,7 @@
 import os
 
 from amino.map import Map
+from amino import Either
 
 
 class EnvVars:
@@ -9,13 +10,13 @@ class EnvVars:
     def vars(self):
         return Map(os.environ)
 
-    def __contains__(self, name):
+    def __contains__(self, name: str) -> bool:
         return name in self.vars
 
-    def __getitem__(self, name):
-        return self.vars.get(name)
+    def __getitem__(self, name: str) -> Either[str, str]:
+        return self.vars.get(name).to_either(f'env var {name} is unset')
 
-    def __setitem__(self, name, value):
+    def __setitem__(self, name: str, value: str) -> None:
         os.environ[name] = str(value)
 
 env = EnvVars()
