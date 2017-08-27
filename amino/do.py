ecad3bfd09069c59bf44157685c13028eb1ed033
Torsten Schmits <torstenschmits@gmail.com>
2017-08-27 20:08:53 +0200

    fix `do` for `StateT`
    Because `StateT` lazily evaluates the functions passed into `flat_map`,
    the ultimate `send` that results in `StopIteration` produces a duplicate
    application of the penultimate result, as that one is returned from the
    `do` iteration into the last `flat_map`.
    
    If this is `StateT.modify`, the function is applied twice.
    
    This change calls `Monad.pure` on the passed in value instead of
    returning the previous effect instance.

--- amino/do.py
+++ amino/do.py
@@ -3,6 +3,7 @@ from typing import TypeVar, Callable, Any, Generator, cast, Optional
 import functools
 
 from amino.tc.base import Implicits
+from amino.tc.monad import Monad
 
 F = TypeVar('F', bound=Implicits)
 A = TypeVar('A')
@@ -15,13 +16,15 @@ def do(f: Callable[..., Generator[F, A, F]]) -> Callable:
         if not isinstance(itr, GeneratorType):
             raise Exception(f'function `{f.__qualname__}` decorated with `do` does not produce a generator')
         c: Optional[F] = None
+        m: Optional[Monad[F]] = None
         def send(val: A) -> F:
-            nonlocal c
+            nonlocal c, m
             try:
                 c = itr.send(val)
+                m = Monad.fatal_for(c)
                 return c.flat_map(send)
             except StopIteration:
-                return cast(F, c)
+                return m.pure(val)
         return send(cast(A, None))
     return do_loop
 
