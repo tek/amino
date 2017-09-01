621e59c205ac8060c503d8e0035131687b63aa8c
Torsten Schmits <torstenschmits@gmail.com>
2017-09-01 19:24:57 +0200

    types

--- amino/test/path.py
+++ amino/test/path.py
@@ -1,7 +1,7 @@
 import sys
-from pathlib import Path
+from typing import Union
 
-from amino import env
+from amino import env, Path
 
 __base_dir__ = None
 
@@ -10,7 +10,7 @@ class TestEnvError(Exception):
     pass
 
 
-def setup(path):
+def setup(path: Union[str, Path]) -> None:
     ''' Use the supplied path to initialise the tests base dir.
     If _path is a file, its dirname is used.
     '''
@@ -26,15 +26,17 @@ def setup(path):
         env['PYTHONPATH'] = '{}:{}'.format(container, env['PYTHONPATH'] | '')
 
 
-def _check():
+def _check() -> Path:
     if __base_dir__ is None:
         msg = 'Test base dir not set! Call amino.test.setup(dir).'
         raise TestEnvError(msg)
+    else:
+        return __base_dir__
 
 
-def temp_path(*components):
-    _check()
-    return Path(__base_dir__, '_temp', *components)
+def temp_path(*components: str) -> Path:
+    dir = _check()
+    return Path(dir, '_temp', *components)
 
 
 def temp_dir(*components: str) -> Path:
@@ -43,32 +45,32 @@ def temp_dir(*components: str) -> Path:
     return _dir
 
 
-def temp_file(*components):
+def temp_file(*components: str) -> Path:
     return temp_dir(*components[:-1]).joinpath(*components[-1:])
 
 
-def create_temp_file(*components):
+def create_temp_file(*components: str) -> Path:
     _file = temp_file(*components)
     _file.touch()
     return _file
 
 
-def fixture_path(*components):
-    _check()
-    return Path(__base_dir__, '_fixtures', *components)
+def fixture_path(*components: str) -> Path:
+    dir = _check()
+    return Path(dir, '_fixtures', *components)
 
 
-def load_fixture(*components):
+def load_fixture(*components: str) -> str:
     with fixture_path(*components).open() as f:
         return f.read()
 
 
-def base_dir():
-    return __base_dir__
+def base_dir() -> Path:
+    return _check()
 
 
 def pkg_dir() -> Path:
     return base_dir().parent
 
-__all__ = ('create_temp_file', 'temp_file', 'temp_path', 'temp_dir',
-           'fixture_path', 'load_fixture', 'base_dir', 'pkg_dir')
+__all__ = ('create_temp_file', 'temp_file', 'temp_path', 'temp_dir', 'fixture_path', 'load_fixture', 'base_dir',
+           'pkg_dir')
