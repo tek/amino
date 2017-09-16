a14e5e1432385b640284161aec809184c483ace0
Torsten Schmits <torstenschmits@gmail.com>
2017-09-16 13:45:32 +0200

    export root logger as `amino_log`

--- amino/__init__.py
+++ amino/__init__.py
@@ -11,8 +11,8 @@ from amino.map import Map
 from amino.future import Future
 from amino.func import curried, I
 from amino.env_vars import env
-from amino.io import Try, IO, IO
-from amino.logging import Logger, log
+from amino.io import Try, IO
+from amino.logging import Logger, amino_root_logger as amino_log
 from amino.eff import Eff
 from amino.eval import Eval
 from amino.regex import Regex
@@ -25,5 +25,5 @@ Path = pathlib.Path
 boot()
 
 __all__ = ('Maybe', 'Just', 'Empty', 'may', 'List', 'Map', '_', 'Future', 'Boolean', 'development', 'flat_may',
-           'curried', '__', 'Left', 'Right', 'Either', 'env', 'Try', 'LazyList', 'Logger', 'log', 'I', 'L', 'Eff',
-           'IO', 'Eval', 'Regex', 'Nothing', 'integration_test', 'Lists', 'do', 'IO', 'Id', 'Nil')
+           'curried', '__', 'Left', 'Right', 'Either', 'env', 'Try', 'LazyList', 'Logger', 'I', 'L', 'Eff',
+           'IO', 'Eval', 'Regex', 'Nothing', 'integration_test', 'Lists', 'do', 'IO', 'Id', 'Nil', 'amino_log')
