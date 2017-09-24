46338d8ea16cbc801df1791b8467ec9268ad8d2e
Torsten Schmits <torstenschmits@gmail.com>
2017-09-24 20:54:07 +0200

    convert do_spec to klk

--- unit/do_spec.py
+++ unit/do_spec.py
@@ -1,37 +1,45 @@
-from typing import Generator
+from typing import Generator, Any
 
 from amino.test.spec_spec import Spec
-from amino import Just, Nothing, Maybe, do
+
+from kallikrein import kf, Expectation, k
+from kallikrein.matchers.maybe import be_just, be_nothing
+from amino import Just, Nothing, Maybe, do, Eval
 from amino.state import EvalState, StateT
 
 
 class DoSpec(Spec):
+    '''do notation
+    yield all `Just`s $just
+    yield a `Nothing` $nothing
+    `EvalState` $eval_state
+    '''
 
-    def just(self) -> None:
+    def just(self) -> Expectation:
         @do
-        def run(i: int) -> Generator[Maybe[int], int, Maybe[int]]:
+        def run(i: int) -> Generator[Maybe[int], Any, None]:
             a = yield Just(i)
             b = yield Just(a + 5)
             c = yield Just(b + 7)
             d = yield Just(c * 3)
             yield Just(d)
-        run(3).should.equal(Just(45))
+        return kf(run, 3).must(be_just(45))
 
     def nothing(self) -> None:
         @do
-        def run(i: int) -> Generator[Maybe[int], int, Maybe[int]]:
+        def run(i: int) -> Generator[Maybe[int], Any, None]:
             yield Just(i)
             b = yield Nothing
             c = yield Just(b + 7)
             yield Just(c)
-        run(3).should.equal(Nothing)
+        return kf(run, 3).must(be_nothing)
 
     def eval_state(self) -> None:
         @do
-        def run() -> None:
+        def run() -> Generator[StateT[Eval, str, Any], Any, None]:
             a = yield EvalState.pure(1)
             yield EvalState.set('state')
             yield EvalState.inspect(lambda s: f'{s}: {a}')
-        run().run_a('init').value.should.equal('state: 1')
+        return k(run().run_a('init').value) == 'state: 1'
 
 __all__ = ('DoSpec',)
