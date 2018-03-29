afeaf969b3951b7dd77828d285fd1a5f3c615661
Torsten Schmits <torstenschmits@gmail.com>
2018-03-29 23:13:19 +0200

    in `snake_case`, don't prefix capital letters following an `_`

--- amino/util/string.py
+++ amino/util/string.py
@@ -7,7 +7,7 @@ import amino
 
 
 def snake_case(name: str) -> str:
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    s1 = re.sub('([^_])([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
