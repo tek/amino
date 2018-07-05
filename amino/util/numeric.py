7532df612b35a5e19ec69c54fdb80601dfe70a13
Torsten Schmits <torstenschmits@gmail.com>
2018-07-05 23:50:04 +0200

    `add`, `sub`

--- amino/util/numeric.py
+++ amino/util/numeric.py
@@ -1,5 +1,5 @@
-import numbers
-from typing import Any
+from numbers import Number
+from typing import Any, Callable
 
 from amino import may, Right, Left, Maybe, Either
 
@@ -7,7 +7,7 @@ from amino import may, Right, Left, Maybe, Either
 @may
 def try_convert_int(data: Any) -> Maybe[int]:
     if (
-            isinstance(data, numbers.Number) or
+            isinstance(data, Number) or
             (isinstance(data, str) and data.isdigit())
     ):
         return int(data)
@@ -19,4 +19,16 @@ def parse_int(i: Any) -> Either[str, int]:
         Left('could not parse int {}'.format(i))
     )
 
-__all__ = ('try_convert_int', 'parse_int')
+
+def add(inc: int) -> Callable[[int], int]:
+    def add(z: int) -> int:
+        return z + inc
+    return add
+
+
+def sub(inc: int) -> Callable[[int], int]:
+    def sub(z: int) -> int:
+        return z - inc
+    return sub
+
+__all__ = ('try_convert_int', 'parse_int', 'add',)
