0c3887112012926a347ecacbf2f19bc56d1cc64c
Torsten Schmits <torstenschmits@gmail.com>
2018-07-29 19:53:10 +0200

    better types for `do`

--- amino/do.py
+++ amino/do.py
@@ -2,12 +2,10 @@ from types import GeneratorType
 from typing import TypeVar, Callable, Any, Generator, Type
 import functools
 
-from amino.tc.base import F
 from amino.tc.monad import Monad
 
 A = TypeVar('A')
 B = TypeVar('B')
-G = TypeVar('G', bound=F)
 Do = Generator
 
 
@@ -15,15 +13,15 @@ Do = Generator
 # itr.gi_frame.f_lasti is the instruction pointer and could be used to detect laziness.
 # NOTE due to the nature of generators, a do with a lazily evaluated monad cannot be executed twice.
 # NOTE Lists don't work properly because the generator will be consumed by the first element
-def do_impl(tpe: type, f: Callable[..., Generator[G, B, None]]) -> Callable[..., G]:
+def do_impl(tpe: type, f: Callable[..., Generator[A, B, A]]) -> Callable[..., A]:
     @functools.wraps(f)
-    def do_loop(*a: Any, **kw: Any) -> F[B]:
+    def do_loop(*a: Any, **kw: Any) -> A:
         m = Monad.fatal(tpe)
         itr = f(*a, **kw)
         if not isinstance(itr, GeneratorType):
             raise Exception(f'function `{f.__qualname__}` decorated with `do` does not produce a generator')
         @functools.wraps(f)
-        def loop(val: B) -> F[B]:
+        def loop(val: B) -> A:
             try:
                 return m.flat_map(itr.send(val), loop)
             except StopIteration as e:
@@ -32,14 +30,14 @@ def do_impl(tpe: type, f: Callable[..., Generator[G, B, None]]) -> Callable[...,
     return do_loop
 
 
-def do(tpe: Type[A]) -> Callable[[Callable[..., Generator]], Callable[..., A]]:
-    def deco(f: Callable[..., Generator]) -> Callable[..., A]:
-        f.tpe = tpe
-        f.__do = None
-        f.__do_original = f
+def do(tpe: Type[A]) -> Callable[[Callable[..., Generator[A, B, A]]], Callable[..., A]]:
+    def deco(f: Callable[..., Generator[A, B, A]]) -> Callable[..., A]:
+        f.tpe = tpe  # type: ignore
+        f.__do = None  # type: ignore
+        f.__do_original = f  # type: ignore
         return functools.wraps(f)(do_impl)(tpe, f)
     return deco
 
 tdo = do
 
-__all__ = ('do', 'F', 'tdo', 'Do')
+__all__ = ('do', 'tdo', 'Do')
